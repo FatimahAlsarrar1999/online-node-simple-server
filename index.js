df6122465608cdde4c1db4506fd9708bb304f88d
unknown <tome19290@gmail.com>
2023-11-08 09:48:05 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,106 @@
-const http = require("http");
-const PORT = '8080'
+import http from 'http'
 
-http
-  .createServer((req, res) => {
-    /* handle http requests */
-  })
-  .listen(PORT);
 
-console.log(`Server running at http://127.0.0.1:${PORT}/`);
+let products = [
+  { id: "0", title: "Tv", price: "9999" },
+  { id: "1", title: "iphone pro max", price: "7000" },
+];
+
+// const http = require("http");
+const PORT = "8080";
+
+
+
+const handleError = (res, statusCode, massage) => {
+  res.writeHead(statusCode, { "Content-Type": "text/plain" })
+  res.write(massage)
+  res.end()
+}
+
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  const MATCH = req.url.match(/\/products\/([0-9]+)/);
+  const ID = req.url?.split('/')[2]
+
+  if (req.url === "/" && req.method === "GET") {
+  try {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>Hello, World!</h1>");
+    res.end();
+    
+  } catch (error) {
+    handleError(res, 500, 'Server Error')
+
+  }
+
+
+  } else if ( MATCH && req.method === "GET") {
+    try {
+      const product = products.find((product) => product.id === ID)
+      if(!product) {
+        handleError(res, 404, `Product not found with ID ${ID}`)
+      }
+      console.log(ID);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(product));
+      res.end();
+    } catch (error) {
+      handleError(res, 500, 'Server Error')
+    }
+
+
+
+  } else if (req.url === "/products" && req.method === "POST") {
+    try {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write('New product is created');
+      res.end();
+    } catch (error) {
+      handleError(res, 500, 'Server Error')
+    }
+
+
+
+  } else if ( MATCH && req.method === "DELETE") {
+    try {
+      const product = products.find((product) => product.id === ID)
+      if(!product) {
+        handleError(res, 404, `Product want deleted not found with ID ${ID}`)
+      }
+      console.log(`product by Id ${ID} is deleted`);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(product));
+      res.end();
+    } catch (error) {
+      handleError(res, 500, 'Server Error')
+    }
+
+
+
+  } else if ( MATCH && req.method === "PUT") {
+    try {
+      const product = products.find((product) => product.id === ID)
+      if(!product) {
+        handleError(res, 404, `Product updated not found with ID ${ID}`)
+      }
+      console.log(`product by Id ${ID} is updated`);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(product));
+      res.end();
+    } catch (error) {
+      handleError(res, 500, 'Server Error')
+    }
+
+
+    
+  }else{
+
+    handleError(res, 404, 'Router is not found')
+  }
+});
+
+
+server.listen(PORT,() =>{
+  console.log(`Server running at http://localhost:${PORT}/`);
+})
